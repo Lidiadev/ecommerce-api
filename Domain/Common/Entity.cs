b151626bf96350329628932b2aff97585464479d
necula_lidia <necula_lidia@yahoo.com>
2020-08-24 20:43:08 +0200

    Add the Entity and ValueObject classes

---
+++ Domain/Common/Entity.cs
@@ -0,0 +1,47 @@
+﻿namespace Domain.Common
+{
+    public abstract class Entity
+    {
+        public long Id { get; protected set; }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (other is null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
---
+++ Domain/Common/ValueObject.cs
@@ -0,0 +1,41 @@
+﻿namespace Domain.Common
+{
+    public abstract class ValueObject<T>
+        where T : ValueObject<T>
+    {
+        protected abstract int GetHashCodeCore();
+
+        protected abstract bool EqualsCore(T other);
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            return EqualsCore(valueObject);
+        }
+
+        public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
+        {
+            return !(a == b);
+        }
+    }
+}
