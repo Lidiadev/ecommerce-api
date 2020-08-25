303a532f92c1caf7c7d02c29b51e6b7c5214e1ae
necula_lidia <necula_lidia@yahoo.com>
2020-08-25 22:09:55 +0200

    Setup dependency injection for application

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
