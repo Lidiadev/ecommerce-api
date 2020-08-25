d3a1eabd29b3c0324ee82d8bd95019a000910389
necula_lidia <necula_lidia@yahoo.com>
2020-08-25 22:11:48 +0200

    Add repo for Customers

---
+++ Domain/Customers/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+using System.Threading.Tasks;
+
+namespace Domain.Customers
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        Task<Customer> AddAsync(Customer customer);
+        Task<Customer> GetAsync(long customerId);
+    }
+}
