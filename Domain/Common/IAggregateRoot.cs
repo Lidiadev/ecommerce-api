a1017becdb0f820738e5ec8e4afb5ca620324075
necula_lidia <necula_lidia@yahoo.com>
2020-08-25 22:07:00 +0200

    Add interfaces for repo and unit of work

---
+++ Domain/Common/IAggregateRoot.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Common
+{
+    public interface IAggregateRoot
+    {
+    }
+}
---
+++ Domain/Common/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
