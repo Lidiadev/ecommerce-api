336a6762c0dba2c12bd1b58d4ddc59b2f05ae5ef
necula_lidia <necula_lidia@yahoo.com>
2020-08-25 22:13:07 +0200

    Create DB context

---
+++ Application/Common/Interfaces/IECommerceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IECommerceDbContext : IUnitOfWork
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
